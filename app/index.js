e06d17f4327e82c134b0b2a45ace034506386a9e
jaykang <keangjay@gmail.com>
2019-09-03 15:19:49 +0800

    删除无用代码

--- app/index.js
+++ app/index.js
@@ -5,30 +5,6 @@ const error = require('koa-json-error') // 错误处理
 const parameter = require('koa-parameter')  // 校验参数
 const app = new Koa();
 
-// const auth = async (ctx, next) => {
-//   if (ctx.url !== '/users') {
-//     ctx.throw(404);
-//   }
-//   await next();
-// }
-
-// app.use(async(ctx, next) => {
-//   console.log(ctx.url);
-//   console.log(ctx.url.match(/\/users/));
-//   console.log(ctx.method);
-//   // console.log(1);
-//   // await next()
-//   // console.log(2);
-//   ctx.body = 'hi, jay!' + ctx.method;
-// });
-// app.use(async(ctx, next) => {
-//   console.log(3);
-//   await next()
-//   console.log(4);
-// })
-// app.use(() => {
-//   console.log(5);
-// })
 app.use(bodyparser());
 app.use(parameter(app))
 app.use(error({
