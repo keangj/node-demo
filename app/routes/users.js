de02343bbded12b933fd37a4d16f5daae7aa3075
jaykang <keangjay@gmail.com>
2019-09-07 12:02:03 +0800

    [feat]使用 koa-jwt 中间件授权认证

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,21 +1,10 @@
 const Router = require('koa-router')
 const router = new Router({prefix: '/users'})
-const jsonwebtoken = require('jsonwebtoken')
+const jwt = require('koa-jwt')
 const { secret } = require('../config')
 const { createUser, getUsers, deleteUser, updateUser, getUser, login, checkOwner } = require('../controllers/users')
 
-const auth = async (ctx, next) => {
-  const { authorization = '' } = ctx.request.headers
-  const token = authorization.replace('Bearer ', '')
-  try {
-    const user = jsonwebtoken.verify(token, secret)
-    console.log(user);
-    ctx.state.user = user
-  } catch (err) {
-    ctx.throw(401, err.message)
-  }
-  await next()
-}
+const auth = jwt({ secret })
 
 router.get('/', getUsers)
 router.get('/:id', getUser)
